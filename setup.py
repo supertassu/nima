070320e9d3dd5ff15c213a8453432f68dc7a2f08
Taavi Väänänen <hi@tassu.me>
2020-12-20 12:23:49 +0200

    Run black on setup.py

--- setup.py
+++ setup.py
@@ -3,30 +3,33 @@ from os import path
 
 current_directory = path.dirname(path.abspath(__file__))
 
-with open(path.join(current_directory, 'README.md'), 'r') as fh:
+with open(path.join(current_directory, "README.md"), "r") as fh:
     long_description = fh.read()
 
-setup(name='nima',
-      version='1.0.0',
-      author='Taavi Väänänen',
-      author_email='hi@taavi.wtf',
-      description='Utility for managing development nginx config files',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/supertassu/nima',
-      install_requires=[line.strip() for line in open(path.join(current_directory, 'requirements.txt'), 'r')],
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.7',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Intended Audience :: System Administrators',
-      ],
-      python_requires='>=3.6',
-
-      packages=['nima'],
-      entry_points='''
+setup(
+    name="nima",
+    version="1.0.0",
+    author="Taavi Väänänen",
+    author_email="hi@taavi.wtf",
+    description="Utility for managing development nginx config files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/supertassu/nima",
+    install_requires=[
+        line.strip()
+        for line in open(path.join(current_directory, "requirements.txt"), "r")
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: System Administrators",
+    ],
+    python_requires=">=3.6",
+    packages=["nima"],
+    entry_points="""
     [console_scripts]
     nima=nima.cli:cli
-''',
-      )
+""",
+)
